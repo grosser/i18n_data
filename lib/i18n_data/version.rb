d521fd90c9eaf877109872cafdfbbeabdaaf0084
Michael Grosser <michael@grosser.it>
2024-09-05 08:28:00 -0700

    v1.1.0

--- lib/i18n_data/version.rb
+++ lib/i18n_data/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module I18nData
-  VERSION = Version = "1.0.0" # rubocop:disable Naming/ConstantName
+  VERSION = Version = "1.1.0" # rubocop:disable Naming/ConstantName
 end
